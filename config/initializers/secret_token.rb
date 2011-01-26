ddba3d45aa30658a393451bfbfc0b9ce97a04609
Chad Sorensen <chdsorensen@gmail.com>
2011-01-25 17:25:36 -0800

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facekick::Application.config.secret_token = '94bb474b874f10e64a42f24df24e6579b0440422204ffd421127d5d056a8f6a1b45447ffeb0cdc5fee5549ed5e29810e303d731c9dceca28a1a2e99ff2764e8b'
