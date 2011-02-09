fe7eca6a5882fc78445389eb3ca3fdbfc8d9bb40
Chad Sorensen <chdsorensen@gmail.com>
2011-02-08 18:00:35 -0800

    finished css bugs, implemented volunteer profile assets on homepage

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,13 @@
 class PublicController < ApplicationController
+  
+  before_filter :load_volunteers
+  
   def index
+    @volunteers = Volunteer.all
+  end
+  
+  def load_volunteers
+    @volunteers = Volunteer.all
   end
 
 end
